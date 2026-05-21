da98545ecfa01f6090fda89106dae00066c27a01
yacan <2377321504@qq.com>
2026-05-21 18:10:25 +0800

    Update API URLs for deployment

--- js/deliveryman.js
+++ js/deliveryman.js
@@ -1,4 +1,4 @@
-const API_URL = 'http://localhost:3000/api';
+const API_URL = 'https://ruanjiangc-work.vercel.app';
 let currentDeliveryman = null;
 let currentOrder = null;
 let refreshInterval = null;
--- js/student.js
+++ js/student.js
@@ -1,4 +1,4 @@
-const API_URL = 'http://localhost:3000/api';
+const API_URL = 'https://ruanjiangc-work.vercel.app';
 let menu = [];
 let cart = [];
 let currentStudent = null;
